808c5ab1109417f75340923b4fc3e8922a1553f0
rahmanaga <78140740+rahmanaga@users.noreply.github.com>
2022-12-21 11:17:27 +0400

    Create basic math operators

---
+++ app.js
@@ -0,0 +1,12 @@
+const add = function (num1, num2) {
+  return num1 + num2;
+};
+const subtract = function (num1, num2) {
+  return num1 - num2;
+};
+const multiply = function (num1, num2) {
+  return num1 * num2;
+};
+const divide = function (num1, num2) {
+  return num1 / num2;
+};
