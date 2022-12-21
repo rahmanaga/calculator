d43acbac9b490cdab8918a6bfe25460ad634c96e
rahmanaga <78140740+rahmanaga@users.noreply.github.com>
2022-12-21 11:24:05 +0400

    Create operate function

--- app.js
+++ app.js
@@ -10,3 +10,22 @@ const multiply = function (num1, num2) {
 const divide = function (num1, num2) {
   return num1 / num2;
 };
+const operate = function (operator, num1, num2) {
+  let result;
+  switch (operator) {
+    case "*":
+      result = multiply(num1, num2);
+      break;
+    case "/":
+      result = divide(num1, num2);
+      break;
+    case "+":
+      result = add(num1, num2);
+      break;
+    case "-":
+      result = subtract(num1, num2);
+      break;
+    default:
+      break;
+  }
+};
